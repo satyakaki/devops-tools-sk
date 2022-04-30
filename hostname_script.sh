d535b49c79eaffa873795faff072e4b7cf7eeaef
nsomesw <Nalla_Someswararao@gap.com>
2022-04-30 18:13:31 +0530

    final test pullrequest

--- hostname_script.sh
+++ hostname_script.sh
@@ -8,3 +8,9 @@ sudo cat << EOF >> /etc/hosts
 192.168.56.11 devops1
 192.168.56.12 devops2
 192.168.56.13 devops3
+
+EOF
+
+echo "ensuring nothing added to /etc/hosts.deny file"
+
+cat /dev/null > /etc/hosts.deny
